b0864e582fe99fb520657802e53e829f48f23642
hhhJG <1207876031@qq.com>
2015-11-29 21:34:14 +0800

    change

--- Project1/Project1/Cluster.h
+++ Project1/Project1/Cluster.h
@@ -7,12 +7,13 @@ using namespace std;
 class Cluster
 {
 private:
-	vector<list<int>> gClusterResult;
+	vector<vector<int>> gClusterResult;
 public:
 	Cluster(void);
 	~Cluster(void);
 	double MaxDisOfTwoSolid(Solid* tFirst, Solid* tSecond);
 	void ClusterByR(vector<Solid>* tSolidVecP, double tR);
 	void SortByLength();
-	vector<list<int>>* GetClusterResult();
+	vector<vector<int>>* GetClusterResult();
+	void DisplayClusterResult();
 };
