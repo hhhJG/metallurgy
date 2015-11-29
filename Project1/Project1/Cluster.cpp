5847f9c651aaef6e47b187dec2c9e4ac0561ed7d
hhhJG <1207876031@qq.com>
2015-11-29 21:33:18 +0800

    change

--- Project1/Project1/Cluster.cpp
+++ Project1/Project1/Cluster.cpp
@@ -6,7 +6,7 @@
 #include <vector>
 #include <list>
 #include <algorithm> 
-
+#include <iostream>
 using namespace std;
 
 
@@ -48,30 +48,59 @@ void Cluster::ClusterByR(vector<Solid>* tSolidVecP, double tR)
 	int mSolidCount = tSolidVecP->size();
 	for(int x = 0; x < mSolidCount; x ++)
 	{
-		list<int> mOneClusterLt;
+		vector<int> mOneClusterVec;
+		//邻居漏洞
 		for(int y = 0; y < mSolidCount; y ++)
 		{
-			if(MaxDisOfTwoSolid(&tSolidVecP->at(x), &tSolidVecP->at(y)) <= tR)
+			if(MaxDisOfTwoSolid(&tSolidVecP->at(x), &tSolidVecP->at(y)) <= tR && x != y)
 			{
-				mOneClusterLt.push_back(tSolidVecP->at(y).GetID());
+				mOneClusterVec.push_back(tSolidVecP->at(y).GetID());
 			}
 		}
-		gClusterResult.push_back(mOneClusterLt);
+		//自身漏洞
+		mOneClusterVec.push_back(tSolidVecP->at(x).GetID());
+
+		gClusterResult.push_back(mOneClusterVec);
 	}
 }
 
 
-bool SortElementCluster(list<int>& tFirst, list<int>& tSecond)
+bool SortElementCluster(vector<int>& tFirst, vector<int>& tSecond)
 {
 	return tFirst.size() > tSecond.size();
 }
 
+/**
+根据聚类之后，每个集合的大小进行排序
+集合中元素多的排在前面，集合中元素少的排在后面
+即从大到小进行排序
+说明：主要是为了用尽可能少的补缩工具补尽可能多的漏洞
+*/
 void Cluster::SortByLength()
 {
 	sort(gClusterResult.begin(), gClusterResult.end(), SortElementCluster);
 }
 
-vector<list<int>>* Cluster::GetClusterResult()
+vector<vector<int>>* Cluster::GetClusterResult()
 {
 	return &(this->gClusterResult);
+}
+
+void Cluster::DisplayClusterResult()
+{
+	cout << "******************聚类结果begin**************************" << endl;
+	vector<vector<int>>::iterator mVecVecIter = this->gClusterResult.begin();
+	while (mVecVecIter != this->gClusterResult.end())
+	{
+		vector<int> mOneClusterVec = *mVecVecIter;
+		vector<int>::iterator  mOneClusterVecIter = mOneClusterVec.begin();
+		while (mOneClusterVecIter != mOneClusterVec.end())
+		{
+			cout << *mOneClusterVecIter << "\t";
+			mOneClusterVecIter++;
+		}
+		cout << endl;
+		mVecVecIter++;
+	}
+	cout << "******************聚类结果end**************************" << endl;
 }
