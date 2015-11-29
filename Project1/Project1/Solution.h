23cfe5720cfbb0f9a855406572dbc6af692e028e
hhhJG <1207876031@qq.com>
2015-11-29 21:34:25 +0800

    change

--- Project1/Project1/Solution.h
+++ Project1/Project1/Solution.h
@@ -4,6 +4,7 @@
 #include <vector>
 #include <string.h>
 #include <list>
+#include <map>
 
 
 struct Riser
@@ -17,14 +18,17 @@ class Solution
 {
 	vector<Riser> gAnswers;
 	vector<Solid>* gAllSolidVec;
-
+	map<int, int> gHaveDoneSolidNumber;//记录以及补缩过的漏洞编号，防止多次补漏
 public:
 	Solution();
 	Solution(vector<Solid>* tAllSolidVec);
 	~Solution();
 	void GetCenterOfSolids(vector<Solid>* tSolidsVec, Point* tResult);
 	string ListIntToString(list<int>* tData);
+	string VectorIntToString(vector<int>* tData);
 	void Method(double tR);
-	void SubMethod(list<int>* tClusteIndexLt, list<int>* tModulusLt);
+	void SubMethod(vector<int>* tClusteIndexLt, double tR);
+	void OneGroupSolidAnswer(vector<Solid>* tSolidsVec, double tR, list<int>* tSameGroupSolidID);
+	void DisplaySolution();
 };
 
